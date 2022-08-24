952548130ddb29a2d3fc14859f8d93ce39608395
Bayron <bayronmuriel@hotmail.com>
2022-08-24 00:43:25 -0500

    fifth commit

--- src/Componets/Inicio.jsx
+++ src/Componets/Inicio.jsx
@@ -7,6 +7,16 @@ function Inicio() {
       <div className='inicionavbar'>
         <Navbar />
       </div>
+      <div className='mainletras'>
+        <div className='letrasinicio'>
+          <h4>ESTÁR CÓMODO,</h4>
+          <h4>NUNCA FUE TAN FÁCIL.</h4>
+        </div>
+        <div className='btn'>
+          {/* <a href=""></a> */}
+          <button type='button'>SHOP</button>
+        </div>
+      </div>
 
     </div>
   )
